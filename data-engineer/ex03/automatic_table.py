4eb8bc4872577d252c592fc43c7fc264e11bbb4a
Muhammed Esad Şeker <muhammed.seker@hepsiburada.com>
2025-02-24 03:01:58 +0300

    Data engineer is completed

--- data-engineer/ex03/automatic_table.py
+++ data-engineer/ex03/automatic_table.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, MetaData, Table
 import sqlalchemy as db
 import os
 
+
 def if_table_exists(engine, table_name):
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -10,13 +11,15 @@ def if_table_exists(engine, table_name):
         print(f"Table {table_name} already exist")
     return table_name in metadata.tables
 
+
 def to_postgre(data : pd.DataFrame, tableName : str):
     engine = db.create_engine('postgresql://museker:123@localhost:5432/piscineds')
     if not if_table_exists(engine=engine, table_name=tableName):
         data.to_sql(
             name=tableName,
-            index=False,
             con=engine,
+            index=False,
+            if_exists="append",
             dtype = {
                 "event_time": db.DateTime(),
                 "event_type": db.types.String(length=255),
@@ -26,19 +29,29 @@ def to_postgre(data : pd.DataFrame, tableName : str):
                 "user_session": db.types.UUID(as_uuid=True)
             }
         )
-        print("the data sended successfully!")
+        print(f"{tableName} sended successfully!")
+
 
 
 def get_data(csv_name : str) -> pd.DataFrame:
     base_dir = os.path.dirname(os.path.abspath(__file__))
     csv_path = os.path.join(base_dir, "..", "..","..","subject", "customer", csv_name)
     csv_path = os.path.abspath(csv_path)
-    data = pd.read_csv(csv_path + ".csv")
+    data = pd.read_csv(csv_path)
     return data
 
+
+def get_and_push():
+    name_list = ["data_2022_dec", "data_2022_nov", "data_2022_oct", "data_2023_jan"]
+    for i in name_list:
+        data = get_data(i + ".csv")
+        to_postgre(data=data, tableName=i)
+        data = pd.DataFrame()
+
+
 def main():
-    data = get_data("data_2022_oct")
-    to_postgre(data, "data_2022_oct")
+    get_and_push()
+
 
 if __name__ == "__main__":
     main()
--- data-engineer/ex04/items_table.py
+++ data-engineer/ex04/items_table.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, MetaData, Table
 import sqlalchemy as db
 import os
 
+
 def if_table_exists(engine, table_name):
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -10,35 +11,45 @@ def if_table_exists(engine, table_name):
         print(f"Table {table_name} already exist")
     return table_name in metadata.tables
 
+
 def to_postgre(data : pd.DataFrame, tableName : str):
     engine = db.create_engine('postgresql://museker:123@localhost:5432/piscineds')
     if not if_table_exists(engine=engine, table_name=tableName):
         data.to_sql(
             name=tableName,
-            index=False,
             con=engine,
+            index=False,
+            if_exists="append",
             dtype = {
-                "event_time": db.DateTime(),
-                "event_type": db.types.String(length=255),
                 "product_id": db.types.Integer(),
-                "price": db.types.Float(),
-                "user_id": db.types.BigInteger(),
-                "user_session": db.types.UUID(as_uuid=True)
+                "category_id": db.types.BigInteger(),
+                "category_code": db.types.String(length=255),
+                "brand": db.types.String(length=255),
             }
         )
-        print("the data sended successfully!")
+        print(f"{tableName} sended successfully!")
+
 
 
 def get_data(csv_name : str) -> pd.DataFrame:
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    csv_path = os.path.join(base_dir, "..", "..","..","subject", "customer", csv_name)
+    csv_path = os.path.join(base_dir, "..", "..","..","subject", "item", csv_name)
     csv_path = os.path.abspath(csv_path)
-    data = pd.read_csv(csv_path + ".csv")
+    data = pd.read_csv(csv_path)
     return data
 
+
+def get_and_push():
+    name_list = ["item"]
+    for i in name_list:
+        data = get_data(i + ".csv")
+        to_postgre(data=data, tableName=i)
+        data = pd.DataFrame()
+
+
 def main():
-    data = get_data("data_2022_oct")
-    to_postgre(data, "data_2022_oct")
+    get_and_push()
+
 
 if __name__ == "__main__":
     main()
